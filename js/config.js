a7f1481e2b996008b1d0751d8b629f6ae40af7a0
Mustapha Bardan <stifbardan600@gmail.com>
2026-05-11 16:16:41 +0200

    another sheet just to be sure

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   protocolVersion: "v2_proposed_methodology_2026_05",
   protocolLabel: "Protocol v2 - proposed methodology",
-  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbx8VIIcAl2c3QbZH81RpBfeT_gglPU4Fek8hTz_s3UBr3D8-xTSHuT8OgPslRW4Q7A3kA/exec",
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbztOko8hIfqh-oAJp9Q9aRLPWiTDOKx6TLMJoMJPsObjnM293KlErOyUvdXYJkAzvG2/exec",
   localStorageKey: "night_walkability_responses_v2",
   progressStorageKey: "night_walkability_progress_v2",
   participantStorageKey: "night_walkability_participant_id_v2",
